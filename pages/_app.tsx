36fe28a16525927931d0bcf1a0f31b9e8182d204
rinku.chaudhary <rinkunited2032@gmail.com>
2023-04-08 20:43:43 +0545

    update: toast notification style

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,7 +11,7 @@ function MyApp({ Component, pageProps }: AppProps) {
 
   return (
     <Fragment>
-      <ToastContainer />
+      <ToastContainer hideProgressBar position="bottom-left"/>
       <RootContext.Provider
         value={{
           userData,
