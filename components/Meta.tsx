7e28f0972c719dec7a0727007504877dcd871201
rinku.chaudhary <rinkunited2032@gmail.com>
2023-04-09 09:55:49 +0545

    update:shortcut icon

--- components/Meta.tsx
+++ components/Meta.tsx
@@ -8,6 +8,7 @@ export default function Meta() {
       <link rel="preconnect" href="https://fonts.googleapis.com" />
       <link rel="preconnect" href="https://fonts.gstatic.com" />
       <link href="https://fonts.googleapis.com/css2?family=Inter&display=swap" rel="stylesheet" />
+      <link rel="shortcut icon" href="https://cms.webo.digital/wp-content/uploads/2021/04/image-1.svg"/>
     </Head>
   );
 }
